e364917410bb6df52e67fa6780bfbbb53731e8d5
aek <anicetkeric@outlook.com>
2025-01-16 20:57:57 +0100

    init code

---
+++ src/main/java/com/boolabs/sample/SpringBootApiQrcodeApplication.java
@@ -0,0 +1,13 @@
+package com.boolabs.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApiQrcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApiQrcodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boolabs/sample/SpringBootApiQrcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boolabs.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApiQrcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
